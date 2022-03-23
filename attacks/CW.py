38f7f6e72570002de1c60307637bd272b08c3b1f
igeng <igeng199684@outlook.com>
2022-03-23 18:47:29 +0800

    Change the one hot encoding method in CW.py.

--- attacks/CW.py
+++ attacks/CW.py
@@ -7,6 +7,8 @@
 @Date    : 2022/3/21 22:20 
 @Descrip :
 '''
+import warnings
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -29,24 +31,29 @@ class CW(Attack):
         self.binary_search_steps = args.cw_binary_search_steps
         self.device = args.device
 
-    def arctanh(self, imgs):
+    def _arctanh(self, imgs):
         scaling = torch.clamp(imgs, min=-1, max=1)
         x = 0.99999999 * scaling
         return 0.5 * torch.log((1 + x) / (1 - x))
 
-    def scaler(self, imgs_atanh):
+    def _scaler(self, imgs_atanh):
         return 0.5 * ((torch.tanh(imgs_atanh)) + 1)
 
     def _f(self, adv, labels):
         outputs = self.target_model(adv)
-        labels_onehot = F.one_hot(labels, 10)
+        # 表示真实的标签的one-hot编码的torch.tensor
+        # labels_onehot = F.one_hot(labels, 10)
+        labels_onehot = torch.eye(len(outputs[0]))[labels].to(self.device)
 
+        # 表示分类模型输出的概率向量中最大的分量概率值
         truth = (labels_onehot * outputs).sum(dim=1)
+        # 表示分类模型输出的概率向量中第二大的分量的概率值
         other, _ = torch.max((1 - labels_onehot) * outputs, dim=1)
 
         if self.targeted:
             # 如果有目标，则labels是要攻击成为得目标，即正常label为1，攻击成label为2
             # 则上文中得labels设置为2，也可以设置为离正常label最远的
+            # -k中k增大是为了增加 other-truth 的置信区间，因为优化目标是最小化other-truth到-k以下
             loss = torch.clamp(other - truth, min=-self.k)
         else:
             loss = torch.clamp(truth - other, min=-self.k)
@@ -58,7 +65,7 @@ class CW(Attack):
         imgs = imgs.clone().detach().to(self.device)
         labels = labels.clone().detach().to(self.device)
 
-        imgs_arctanh = self.arctanh(imgs)
+        imgs_arctanh = self._arctanh(imgs)
 
         for _ in range(self.binary_search_steps):
             perturbation = torch.zeros_like(imgs).to(self.device)
@@ -69,7 +76,7 @@ class CW(Attack):
 
             for step in range(self.n_iters):
                 optimizer.zero_grad()
-                adv_examples = self.scaler(imgs_arctanh + perturbation)
+                adv_examples = self._scaler(imgs_arctanh + perturbation)
                 loss1 = torch.sum(self.c * self._f(adv_examples, labels))
                 loss2 = F.mse_loss(adv_examples, imgs, reduction='sum')
 
@@ -79,10 +86,12 @@ class CW(Attack):
 
                 if step % (self.n_iters // 10) == 0:
                     if loss > prev_loss:
+                        # warnings.warn("Early stopped because the loss didn't converged.")
+                        # return self.scaler(imgs_arctanh + perturbation).detach()
                         break
                     prev_loss = loss
 
-            adv_imgs = self.scaler(imgs_arctanh + perturbation).detach()
+            adv_imgs = self._scaler(imgs_arctanh + perturbation).detach()
             return adv_imgs
 
 
