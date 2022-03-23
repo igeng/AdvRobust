edcbc670b1b376248d60ce9adf9e3f4bc9594a19
igeng <igeng199684@outlook.com>
2022-03-23 22:02:35 +0800

    Add "L2" to CW.py because CW is also L2 norm attack method.

--- attacks/CWL2.py
+++ attacks/CWL2.py
@@ -56,7 +56,7 @@ class CWL2(Attack):
             adv_imgs = self.tanh(w)
 
             current_L2 = MSELoss(Flatten(adv_imgs),
-                                 Flatten(imgs)).sum(dim=1)
+                                 Flatten(imgs)).sum(dim=1) # torch.Size([100, 3072]).sum(dim=1) ==> torch.Size([100])
             L2_loss = current_L2.sum()
 
             outputs = self.target_model(adv_imgs)
@@ -69,12 +69,12 @@ class CWL2(Attack):
 
             _, pre = torch.max(outputs.detach(), 1)
             correct = (pre == labels).float()
-
+            # mask : both misclassified and current_L2 loss decreases. current_L2 is smaller to be best_L2
             mask = (1 - correct) * (best_L2 > current_L2.detach())
             best_L2 = mask * current_L2.detach() + (1 - mask) * best_L2
 
-            mask = mask.view([-1] + [1] * (dim - 1))
-            best_adv_imgs = mask * adv_imgs.detach() + (1 - mask) * best_adv_imgs
+            mask = mask.view([-1] + [1] * (dim - 1)) # [-1, 1, 1, 1]
+            best_adv_imgs = mask * adv_imgs.detach() + (1 - mask) * best_adv_imgs # 保留adv_imgs中被错分且current_L2更小的图片[100,1,1,1]中最小维度里的1或0对应[100,3,32,32]中一个[:,3,32,32]
 
             if step % (self.n_iters // 10) == 0:
                 if loss > prev_loss:
