93d3b71f2e432533364efb97ef27e83bfd70ac06
igeng <igeng199684@outlook.com>
2022-04-09 12:35:33 +0800

    Add some new attack methods.

--- attacks/__init__.py
+++ attacks/__init__.py
@@ -7,13 +7,24 @@
 @Date    : 2022/3/21 17:04 
 @Descrip :
 '''
+from .CLEAN import *
 from .Attacks import *
 from .FGSM import *
+from .FGNM import *
 from .PGD import *
-from .WRNM_PGD import *
-from .WRNM_PGD_LTG import *
+from .PGND import *
+from .WRPGD import *
+from .EWRPGD import *
+from .MTPGD import *
+from .MSPGD import *
+from .WRNM_PGD_Vanila import * # zhenghuabin版本 原生的nesterov
+from .WRNM_PGD_LTG import * # ZhengHuabin版本的差值变形
+from .WRNMM import * # zhenghuabin版本的wangyun等效变换
+from .WRNMM_equal import * # wangyun版本的等效变换
 from .PGDL2 import *
 from .BIM import *
 from .MIM import *
 from .CW import *
 from .CWL2 import *
+from .LookAhead import *
+from .LookAheadAdam import *
